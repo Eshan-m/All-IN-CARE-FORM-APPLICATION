640727ba90b9501097a7d5f91c6f22185a3f8662
ESHAN <57050637+Eshan-m@users.noreply.github.com>
2020-07-04 10:40:42 +0530

    Update Pharmacy.Designer.cs

--- Pharmacy.Designer.cs
+++ Pharmacy.Designer.cs
@@ -39,6 +39,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // splitter1
@@ -137,11 +138,22 @@
             this.label4.TabIndex = 9;
             this.label4.Text = "Select Pharmacy";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(494, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(218, 31);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Payment method";
+            // 
             // Pharmacy
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
@@ -172,5 +184,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
