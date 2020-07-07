4ae6f9ee65496e0ef686a42bacd7968cac52abe0
ESHAN <57050637+Eshan-m@users.noreply.github.com>
2020-07-07 10:18:32 +0530

    Update DPLOGIN.Designer.cs

--- DPLOGIN.Designer.cs
+++ DPLOGIN.Designer.cs
@@ -96,7 +96,7 @@
             this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(303, 398);
+            this.button3.Location = new System.Drawing.Point(302, 397);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(123, 34);
             this.button3.TabIndex = 4;
