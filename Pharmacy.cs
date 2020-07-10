2779ad16c759526a2c43adc71444cfb08e0cb691
shanon25 <59134659+shanon25@users.noreply.github.com>
2020-07-10 19:14:39 +0530

    Added a database to the Pharmacy.cs

--- Pharmacy.cs
+++ Pharmacy.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,14 +13,35 @@ namespace WindowsFormsApp1
 {
     public partial class Pharmacy : Form
     {
+         
         public Pharmacy()
         {
             InitializeComponent();
         }
 
+        string Payment;
         private void button1_Click(object sender, EventArgs e)
         {
+            
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ASUS\Desktop\All-IN-CARE-FORM-APPLICATION\Pharmacy.mdf;Integrated Security=True;Connect Timeout=30");
+            string query = "insert into Pharmacy(City,Pharmacy,Date,Payment) values('" + cityCmb.Text + "','" + pharmacyLst.Text + "','" + dateCal.CustomFormat + "','" + Payment + "')";
+            SqlCommand cmd = new SqlCommand(query, con);
             this.Hide();
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Records were added successfully");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+                MessageBox.Show("Error your values didn't added");
+            }
+            finally
+            {
+                con.Close();
+            }
         }
 
         private void label4_Click(object sender, EventArgs e)
@@ -36,6 +58,21 @@ namespace WindowsFormsApp1
         {
            
            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cashBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            Payment = "Cash On Delivery";
+        }
+
+        private void payBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            Payment = "Pay Now";
         }
     }
 }
