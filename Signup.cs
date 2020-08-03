3df4976bb202200dc611173e6ab7e12ef4a570ad
shanon25 <59134659+shanon25@users.noreply.github.com>
2020-08-03 17:28:18 +0530

    Bug fixes and added some features

--- Signup.cs
+++ Signup.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp1
 {
@@ -15,71 +16,108 @@ namespace WindowsFormsApp1
         public Signup()
         {
             InitializeComponent();
-        }
-
-        private void SelectLogin_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
+        }       
 
         private void button1_Click(object sender, EventArgs e)
         {
             this.Hide();
-            //Patient sql
-            /*SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\EDUCATION\1 Year SEM 3\Object Oriented Programming with C#\Assigments\SEM END Project\All-IN-CARE-FORM-APPLICATION\AllInCare.mdf;Integrated Security=True;Connect Timeout=30");
-            String query = "update PatientSettings where UserName='" + Username.Text + "'set FirstName='" + FName.Text + "'set LastName='" + LName.Text + "' set BirthDate='" + BDdate.Value.ToString() + "'set AdressLine1='" + AdLine1.Text + "'set AdressLine2='" + ADLine2.Text + "'set ZipCode='" + ZipCode.Text + "'set Province='" + Province.Text + "'set Email='" + Mail.Text + "' set MobileNo1='" + MbNo1.Text + "' set MobileNo2='" + MbNo2.Text + "' set EmergencyNo1='" + ECNo1.Text + "' set EmergencyNo2='" + ECNo2.Text + "'";
-            SqlCommand cmd = new SqlCommand(query, con);
-            this.Hide();
-            Patient log = new Patient();
-            log.Show();
-            try
+            string option;
+            option = loginCmb.Text;
+            switch (option)
             {
-                con.Open();
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Your Records Updated Successfully");
+                case "Patient ":
+                    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ASUS\Desktop\All-IN-CARE-FORM-APPLICATION\AllInCare.mdf;Integrated Security=True;Connect Timeout=30");
+                    string query = "insert into PatientSettings(Username,FirstName,LastName,BirthDate,AdressLine1,AdressLine2,ZipCode,Province,Email,MobileNo1,Password,ConfirmPassword) values('" + unameTxt.Text + "','" + fnameTxt.Text + "','" + lnameTxt.Text + "','" + bdayCal.Value + "','" + ad1Txt.Text + "','" + ad2Txt.Text + "','" + zipTxt.Text + "','" + provCmb.Text + "','" + emailTxt.Text + "','" + mobileTxt.Text + "','" + pwTxt.Text + "','" + conpwTxt + "')";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    try
+                    {
+                        if (pwTxt.Text == conpwTxt.Text)
+                        {
+                            con.Open();
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Your Records Updated Successfully");
+                        }
+                        else
+                        {
+                            MessageBox.Show("The password you entered is not matching with the previous one");
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("TRY AGAIN ERROR OCCURED!!");
+                        Console.Write(ex.ToString());
+                    }
+                    finally
+                    {
+                        con.Close();
+                    }
+                    break;
+
+                case "Doctor":
+                    SqlConnection con2= new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ASUS\Desktop\All-IN-CARE-FORM-APPLICATION\AllInCare.mdf;Integrated Security=True;Connect Timeout=30");
+                    string query2 = "insert into DoctorSettings(Username,FirstName,LastName,BirthDate,AddressLine1,AddressLine2,ZipCode,Province,Email,MobileNo1,Password,ConfirmPassword) values('" + unameTxt.Text + "','" + fnameTxt.Text + "','" + lnameTxt.Text + "','" + bdayCal.Value + "','" + ad1Txt.Text + "','" + ad2Txt.Text + "','" + zipTxt.Text + "','" + provCmb.Text + "','" + emailTxt.Text + "','" + mobileTxt.Text + "','" + pwTxt.Text + "','" + conpwTxt + "')";
+                    SqlCommand cmd2 = new SqlCommand(query2,con2);
+                    try
+                    {
+                        if (pwTxt.Text == conpwTxt.Text)
+                        {
+                            con2.Open();
+                            cmd2.ExecuteNonQuery();
+                            MessageBox.Show("Your Records Updated Successfully");
+                        }
+                        else
+                        {
+                            MessageBox.Show("The password you entered is not matching with the previous one");
+                        }
+                        
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("TRY AGAIN ERROR OCCURED!!");
+                        Console.Write(ex.ToString());
+                    }
+                    finally
+                    {
+                        con2.Close();
+                    }
+                    break;
+                case "Pharmarcy":
+                    MessageBox.Show("Thank you for connecting with us");
+                    break;
+                case "Rider":
+                    MessageBox.Show("Thank you for connecting with us");
+                    break;
+                default:
+                    MessageBox.Show("error");
+                    break;
+
             }
-            catch (SqlException ex)
+           
+        }
+
+        private void loginCmb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string option;
+            option = loginCmb.Text;
+            switch (option)
             {
-                MessageBox.Show("TRY AGAIN ERROR OCCURED!!");
-                Console.Write(ex.ToString());
-
-
+                case "Patient ":
+                    MessageBox.Show("You have selected Patient do you wanna proceed");
+                    break;
+                case "Doctor":
+                    MessageBox.Show("You have selected Doctor do you wanna proceed");
+                    break;
+                case "Pharmarcy":
+                    MessageBox.Show("You have selected Pharmacy do you wanna proceed");
+                    break;
+                case "Rider":
+                    MessageBox.Show("You have selected Rider do you wanna proceed");
+                    break;
+                default:
+                    MessageBox.Show("error");
+                    break;
             }
-            finally
-            {
-                con.Close();
-            }*/
-
-            //Doctor Sql
-            /*SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\EDUCATION\1 Year SEM 3\Object Oriented Programming with C#\Assigments\SEM END Project\All-IN-CARE-FORM-APPLICATION\AllInCare.mdf;Integrated Security=True;Connect Timeout=30");
-            String query = "update DoctorSettings where Username='" + Username.Text + "'set FirstName='" + Fname.Text + "' set LastName='" + Lname.Text + "'set BirthDate='" + BDdate.Value.ToString() + "'set AddressLine1='" + ADLine1.Text + "'set AddressLine2='" + ADLine2.Text + "'set Zipcode='" + ZipCode.Text + "' set Province='" + Province.Text + "' set Email='" + Mail.Text + "' set MobileNo1='" + MN1.Text + "'set MobileNo2='" + MN2.Text + "'set EmergencyNo1='" + EC1.Text + "'set EmergencyNo2='" + EC2.Text + "'";
-            SqlCommand cmd = new SqlCommand(query, con);
-            this.Hide();
-            Doctor log = new Doctor();
-            log.Show();
-            try
-            {
-                con.Open();
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Your Records Updated Successfully");
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show("TRY AGAIN ERROR OCCURED!!");
-                //Console.Write(ex.ToString());
-                MessageBox.Show(ex.ToString());
-
-
-            }
-            finally
-            {
-                con.Close();
-            }*/
-
-
-
-}
-
-       
+        }
+  
     }
 }
